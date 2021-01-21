35c5d77889dfc39836af7e77c94cb0be2989a3ee
AlissonCpx <alissondeandrade1@gmail.com>
2021-01-21 17:59:54 -0300

    Enviando arquivos

---
+++ android/app/src/main/kotlin/com/alisson/lista_de_tarefas_v2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alisson.lista_de_tarefas_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:lista_de_tarefas_v2/src/ui/home.dart';
+import 'package:lista_de_tarefas_v2/src/ui/inicialHome.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: InicialHome(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
